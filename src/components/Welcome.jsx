1dbc165ebde66aa441cfce06aad819c08fda4bf7
Adam Green <greenap89@gmail.com>
2023-08-15 13:57:17 -0400

    added initial openai routes and functionality

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,68 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 
 const Welcome = () => {
+    const [ value, setValue ] = useState('')
+    const [ message, setMessage ] = useState(null)
+    const [ previousChats, setPreviousChats ] = useState([])
+    const [ currentTitle, setCurrentTitle ] = useState(null)
+
+
+    const getMessages = async () => {
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify({
+                message: value
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const response = await fetch('http://localhost:5000/completions', options)
+            const data = await response.json()
+            console.log(data)
+            setMessage(data.choices[0].message)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        if(!currentTitle && value && message) {
+            setCurrentTitle(value)
+        }
+        if(currentTitle && value && message) {
+            setPreviousChats(prevChats => (
+                [...prevChats, 
+                    {
+                        title: currentTitle, 
+                        role: 'user',
+                        content: value
+                    }, 
+                    
+                    {
+                        title: currentTitle, 
+                        role: 'user',
+                        content: message.content
+                    }
+                ]
+        ))
+        }
+    }, [message, currentTitle])
+
     return (
         <div>
-            Welcome! your journey is under construction!
+            <div>
+                Welcome! your journey is under construction!
+            </div>
+            <div className="input-container">
+                <input value={value} onChange={(e) => setValue(e.target.value)}/>
+                <button id="submit" onClick={getMessages}>Submit</button>
+            </div>
         </div>
     )
 }
