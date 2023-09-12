4b2e63778b75cfec4864a1c2e7dd36fd8926b2d6
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-09-11 22:25:32 -0400

    dashboard almost done

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,7 +6,19 @@ import { Link } from "react-router-dom"
 
 const Dashboard = (props) => {
     const [plans, setPlans] = useState([])
+    //const [finished, setFinished] = useState([])
     
+   
+    let finished = plans.map((plan) =>{
+        return plan.tasks.map(task => {
+        return task.action_items.every((actionItem) => actionItem.completed)
+        }).every(plan => plan === true) ? 1: 0
+    }).reduce((t,c) => t = t + c,0)
+
+    let openPlans = plans.length - finished
+    console.log('finished',finished)
+    
+
     useEffect(() => {
         
         if(props.uId) {
@@ -37,20 +49,29 @@ const Dashboard = (props) => {
                         </div>
                         <div className="statItem">
                             <h3>Plans Finished</h3>
-                            <div className="planStat">PH</div>
+                            <div className="planStat">{finished}</div>
                         </div>
                         <div className="statItem">
                             <h3>Plans Open</h3>
-                            <div className="planStat">PH</div>
+                            <div className="planStat">{openPlans}</div>
                         </div>
                     </div>
-                    <div>Completion Ratio %</div>
+                    <div>Completion Ratio {Math.round((finished/plans.length)*100)}%</div>
                 </div>
                 <nav className="existingPlans">
                     <h2>Existing plans</h2>
                     {plans.length? plans.map((plan,idx) => {
                         return (
-                            <Link to='/actionplan' state={{ message: plan }} key={idx} className="planButton">{plan.title}</Link>
+                            <Link to='/actionplan' state={{ message: plan }} key={idx} className="planButton">
+                                {Math.round(plan.tasks.reduce((total, task) => {
+                                    return (
+                                        total +
+                                        task.action_items.reduce((taskTotal, actionItem) => {
+                                        return taskTotal + (actionItem.completed ? 1 : 0);
+                                        }, 0)
+                                    );
+                                    }, 0)/plan.tasks.reduce((total, task) => total +
+                                    task.action_items.length,0)* 100)}% {plan.title}</Link>
                         )
                     })
                     : <div>You have no plans, create one!</div>}
