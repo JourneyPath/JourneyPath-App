c3a4a8bbfacba422e294e5cfef4856e71bce9f7e
Adam Green <greenap89@gmail.com>
2023-09-19 11:29:10 -0400

    updated save button modal

--- public/components/SaveButton.jsx
+++ public/components/SaveButton.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import { app as firebaseApp } from "../../functions/firebaseConfig"
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import { auth } from "../../functions/firebaseConfig";
 
 const SaveButton = (props) => {
-    const db = getFirestore(firebaseApp)
-    
+    const db = getFirestore(firebaseApp);
+    const [isSaveModalOpen, setIsSaveModalOpen] = useState(false);
+
     const saveMessage = async (messageToSave) => {
-        console.log(messageToSave)
+        console.log(messageToSave);
         if (auth.currentUser) {
             await setDoc(doc(db, auth.currentUser.uid, messageToSave.title), messageToSave);
         } else {
-            console.log("you're not logged in!")
+            console.log("You're not logged in!");
         }
     }
 
+    const handleSaveButtonClick = () => {
+        saveMessage(props.message);
+        setIsSaveModalOpen(true);
+    }
+
     return (
-        <button onClick={() => saveMessage(props.message)}>Save Action Plan</button>
+        <div>
+            <button onClick={handleSaveButtonClick}className="save-action-button">Save Action Plan</button>
+            {isSaveModalOpen && (
+                <div className="saveModal">
+                    <div className="saveModal-content">
+                        <p>Your plan has been saved and can be found in your dashboard!</p>
+                        <button onClick={() => setIsSaveModalOpen(false)}>Ok</button>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
-export default SaveButton
+export default SaveButton;
